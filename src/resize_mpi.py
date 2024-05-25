1a6256eb0e2042b71163edb1e1e75cb458e181ab
Andrey170170 <24122004@bk.ru>
2024-05-24 20:51:29 -0400

    Parquet output for resizer
    Changed method of storage for resizer - from 7z to parquet

--- src/resize_mpi.py
+++ src/resize_mpi.py
@@ -1,6 +1,5 @@
 import hashlib
 import os
-import tempfile
 import time
 from typing import Tuple
 
@@ -13,15 +12,14 @@ import py7zr
 from PIL import Image, UnidentifiedImageError
 from py7zr import FILTER_ZSTD
 
-
 comm = MPI.COMM_WORLD
 rank = comm.rank
-output_path = "/users/PAS2119/andreykopanev/gbif/data/verification_stat"
-schedule_path = "/users/PAS2119/andreykopanev/gbif/data/schedule_full.csv"
+output_path = "/users/PAS2119/andreykopanev/distributed-downloader/data/verification_stat"
+schedule_path = "/users/PAS2119/andreykopanev/distributed-downloader/data/schedule_full.csv"
 base_path = "/fs/scratch/PAS2136/gbif/processed/2024-05-01/multimedia_prep/downloaded_images"
 _new_size = 720
-read_time = 900
-write_time = 600
+read_time = 150
+write_time = 150
 schedule_df = pd.read_csv(schedule_path)
 schedule = schedule_df.query(f"Rank == {rank}").set_index("ServerName").to_dict("index")
 
@@ -103,7 +101,8 @@ def resize_partition(partition: pd.DataFrame) -> pd.DataFrame:
         return pd.DataFrame(columns=["uuid", "identifier", "ServerName", "verified", "verification_msg"])
 
     if time.time() > int(os.getenv("SLURM_JOB_END_TIME", 0)) - (write_time + read_time):
-        print(f"Not enough time to resize {int(os.getenv('SLURM_JOB_END_TIME', 0)) - time.time()} left, {write_time + read_time} needed")
+        print(
+            f"Not enough time to resize {int(os.getenv('SLURM_JOB_END_TIME', 0)) - time.time()} left, {write_time + read_time} needed")
         return pd.DataFrame(columns=["uuid", "identifier", "ServerName", "verified", "verification_msg"])
 
     partition_dict = partition.to_dict("index")
@@ -111,61 +110,48 @@ def resize_partition(partition: pd.DataFrame) -> pd.DataFrame:
     verification_dict = {}
 
     try:
-        with tempfile.TemporaryDirectory() as resized_images_dir:
-            resized_dir = f"{resized_images_dir}/resized"
-            os.mkdir(resized_dir)
+        with py7zr.SevenZipFile(f"{partition_path}/images.7z", 'r', filters=[{'id': FILTER_ZSTD, 'level': 3}]) as f:
+            names = f.getnames()
+            for fname, bio in f.read(names).items():
+                image_stream = bio.read()
 
-            with py7zr.SevenZipFile(f"{partition_path}/images.7z", 'r', filters=[{'id': FILTER_ZSTD, 'level': 3}]) as f:
-                print("opened file")
-                for fname, bio in f.readall().items():
-                    image_stream = bio.read()
+                image_shape: np.ndarray[int, np.dtype[np.int32]] = partition_dict[fname]["resized_size"]
+                image_original_np: np.ndarray = np.frombuffer(image_stream, dtype=np.uint8).reshape(
+                    [image_shape[0], image_shape[1], 3])
 
-                    image_shape: np.ndarray[int, np.dtype[np.int32]] = partition_dict[fname]["resized_size"]
-                    image_original_np: np.ndarray = np.frombuffer(image_stream, dtype=np.uint8).reshape(
-                        [image_shape[0], image_shape[1], 3])
+                is_valid, error_msg = validate_image_data(image_stream, image_shape,
+                                                          partition_dict[fname]["hashsum_resized"])
+                verification_dict[fname] = {
+                    "identifier": partition_dict[fname]["identifier"],
+                    "ServerName": server_name,
+                    "verified": is_valid,
+                    "verification_msg": error_msg}
 
-                    is_valid, error_msg = validate_image_data(image_stream, image_shape,
-                                                              partition_dict[fname]["hashsum_resized"])
-                    verification_dict[fname] = {
-                        "identifier": partition_dict[fname]["identifier"],
-                        "ServerName": server_name,
-                        "verified": is_valid,
-                        "verification_msg": error_msg}
+                if image_shape[0] > _new_size or image_shape[1] > _new_size:
+                    image_original_np, image_shape = image_resize(image_original_np, _new_size)
 
-                    if image_shape[0] > _new_size or image_shape[1] > _new_size:
-                        image_original_np, image_shape = image_resize(image_original_np, _new_size)
-
-                    image_stream = image_original_np.tobytes()
-                    partition_dict[fname]["hashsum_resized"] = hashlib.md5(image_stream).hexdigest()
-                    partition_dict[fname]["resized_size"] = image_shape
-
-                    with open(f"{resized_dir}/{fname}", "wb") as file:
-                        file.write(image_stream)
-
-            if time.time() > int(os.getenv("SLURM_JOB_END_TIME", 0)) - write_time:
-                print(f"Not enough time to write {int(os.getenv('SLURM_JOB_END_TIME', 0)) - time.time()} left, {write_time} needed")
-                return pd.DataFrame(columns=["uuid", "identifier", "ServerName", "verified", "verification_msg"])
-
-            with py7zr.SevenZipFile(f"{partition_path}/images.7z", 'w', filters=[{'id': FILTER_ZSTD, 'level': 3}]) as f:
-                for fname in os.listdir(resized_dir):
-                    f.write(f"{resized_dir}/{fname}", fname)
-
+                image_stream = image_original_np.tobytes()
+                partition_dict[fname]["hashsum_resized"] = hashlib.md5(image_stream).hexdigest()
+                partition_dict[fname]["resized_size"] = image_shape
+                partition_dict[fname]["image"] = image_stream
     except Exception as e:
         corrupted = open(f"{partition_path}/_corrupted.txt", "w")
         print(str(e), file=corrupted)
         corrupted.close()
+        print(f"Error: {server_name}: {e}", flush=True)
         return pd.DataFrame(columns=["uuid", "identifier", "ServerName", "verified", "verification_msg"])
     else:
+        os.remove(f"{partition_path}/images.7z")
         (pd.DataFrame
          .from_dict(partition_dict, orient="index")
          .reset_index(names="uuid")
          .drop(columns=["ServerName", "partition_id"])
-         .to_parquet(f"{partition_path}/successes.parquet", index=False))
+         .to_parquet(f"{partition_path}/successes.parquet", index=False, compression="zstd", compression_level=3))
 
         verification_df = pd.DataFrame.from_dict(verification_dict, orient="index").reset_index(names="uuid")
 
-        (verification_df.drop(columns=["identifier", "ServerName"])
-         .to_parquet(f"{partition_path}/verification.parquet", index=False))
+        verification_df.drop(columns=["identifier", "ServerName"]).to_parquet(f"{partition_path}/verification.parquet",
+                                                                              index=False)
 
         return verification_df
 
